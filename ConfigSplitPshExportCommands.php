5613a7d3a601d23b4e9b6284e864e671396bc217
Tassilo Gröper <tassilogroeper@wearewondrous.com>
2019-04-26 11:25:25 +0200

    v1.0.0 ✨

---
+++ ConfigSplitPshExportCommands.php
@@ -0,0 +1,28 @@
+<?php
+namespace Drush\Commands\config_split_psh_export;
+
+use Drush\Commands\DrushCommands;
+use Consolidation\AnnotatedCommand\CommandData;
+
+/**
+ *
+ */
+class ConfigSplitPshExportCommands extends DrushCommands {
+
+  const HOOK_FUNCTION = 'preCommandConfigSplitPshExport';
+
+  /**
+   * Hooks into config-export drush command and alters it's behavior.
+   *
+   * @hook pre-command config:export
+   *
+   * @param \Consolidation\AnnotatedCommand\CommandData $commandData
+   */
+  public function preCommand(CommandData $commandData) {
+    if (!function_exists(self::HOOK_FUNCTION)) {
+      return;
+    }
+
+    call_user_func(self::HOOK_FUNCTION, $commandData);
+  }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Config Split Platform.sh Export
+
+Drush 9 alter hook to use production config during the config:export command on platform.sh environment.
+only works with [Psh-toolbelt](https://github.com/wearewondrous/psh-toolbelt) setup!
+
+## Installation
+
+No need to directly install this module. Follow install instructions here:
+[https://github.com/wearewondrous/psh-toolbelt](https://github.com/wearewondrous/psh-toolbelt)
